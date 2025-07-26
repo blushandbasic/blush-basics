323881c32843aecfebf6e0bd3fcbd94dc93e4d6a
Blush & Basics <sumaiyasyedh@gmail.com>
2025-07-26 09:57:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ body {
 }
 
 #hero {
-  background-image: url('images/hero-image.jpg');
+  background-image: url('./hero-image.jpg');
   background-size: cover;
   text-align: center;
   color: white;
